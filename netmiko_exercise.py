af5a8e3be7cdff608d126608f5741e1b2a52d692
Patrick Herrington <pherrington3@gatech.edu>
2020-05-03 10:24:55 -0700

    updates to netmiko exercises.  exercise 3 answer added.

--- netmiko_exercise.py
+++ netmiko_exercise.py
@@ -17,9 +17,22 @@ nxos2 = {
     "password": password,
     "device_type": "cisco_nxos"
 }
+cisco3 = {
+    "host": "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_ios"
+}
 
 device_list = (nxos1, nxos2)
 for device in device_list:
     net_connect = ConnectHandler(**device)
     print(net_connect.find_prompt())
     net_connect.disconnect()
+
+net_connect= ConnectHandler(**cisco3)
+output = net_connect.send_command('show version')
+
+fout = open('show_version.txt', 'w')
+fout.write(output)
+net_connect.disconnect()
