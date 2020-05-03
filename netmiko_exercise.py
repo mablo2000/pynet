c3cecaf2e0b0bcf3455c7961f00281e6f6cf3662
Patrick Herrington <pherrington3@gatech.edu>
2020-05-03 10:19:18 -0700

    updates to netmiko exercises.  exercise 2 answer.

--- netmiko_exercise.py
+++ netmiko_exercise.py
@@ -1,4 +1,25 @@
+#!/usr/bin/env python
+
 from netmiko import ConnectHandler
 from getpass import getpass
 
+password = getpass()
 
+nxos1 = {
+    "host": "nxos1.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_nxos"
+}
+nxos2 = {
+    "host": "nxos2.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_nxos"
+}
+
+device_list = (nxos1, nxos2)
+for device in device_list:
+    net_connect = ConnectHandler(**device)
+    print(net_connect.find_prompt())
+    net_connect.disconnect()
