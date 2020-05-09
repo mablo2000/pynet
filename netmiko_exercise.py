724f0a4a5ba409a3da0bba810f203cbf766aad2a
Patrick Herrington <pherrington3@gatech.edu>
2020-05-09 10:18:14 -0700

    raw strings for expect_string in 1b

--- netmiko_exercise.py
+++ netmiko_exercise.py
@@ -31,13 +31,13 @@ cisco4 = {
 }
 
 net_connect= ConnectHandler(**cisco4)
-output = net_connect.send_command('ping', strip_prompt=False, strip_command=False, expect_string='Protocol')
-output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string='IP address')
-output += net_connect.send_command('8.8.8.8', strip_prompt=False, strip_command=False, expect_string='Repeat')
-output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string='Datagram')
-output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string='Timeout')
-output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string='Extended')
-output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string='Sweep')
+output = net_connect.send_command('ping', strip_prompt=False, strip_command=False, expect_string=r'Protocol')
+output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string=r'IP address')
+output += net_connect.send_command('8.8.8.8', strip_prompt=False, strip_command=False, expect_string=r'Repeat')
+output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string=r'Datagram')
+output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string=r'Timeout')
+output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string=r'Extended')
+output += net_connect.send_command('\n', strip_prompt=False, strip_command=False, expect_string=r'Sweep')
 output += net_connect.send_command('\n', strip_prompt=False, strip_command=False)
 net_connect.disconnect()
 
