40b329d73bdf20525e832bc15e59755ff829ecbf
Patrick Herrington <pherrington3@gatech.edu>
2020-05-18 11:17:09 -0700

    ex5

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -4,8 +4,13 @@ PyPlus Class 3 Exercises
 """
 from pprint import pprint
 import yaml
+from netmiko import ConnectHandler
+
 
 myfile = '../.netmiko.yml'
 with open(myfile, 'r') as fin:
     device_list = yaml.load(fin)
-pprint(device_list)
+cisco3 = device_list['cisco3']
+net_connect = ConnectHandler(**cisco3)
+output = net_connect.find_prompt()
+pprint(output)
