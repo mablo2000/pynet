6905dc081714ea788cb387c1061839b5c61f340b
Patrick Herrington <pherrington3@gatech.edu>
2020-05-17 17:07:48 -0700

    ex 2b

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -3,6 +3,7 @@
 PyPlus Class 3 Exercises
 """
 from pprint import pprint
+import yaml
 
 device_list = [
     {'device_name': 'cisco3', 'hostname': 'cisco3.lasthop.io', 'username': 'myname', 'password': 'mypass'},
@@ -11,4 +12,11 @@ device_list = [
     {'device_name': 'arista2', 'hostname': 'arista2.lasthop.io', 'username': 'myname', 'password': 'mypass'},
 ]
 
+myfile = 'device_list.yaml'
+with open('w', myfile) as fout:
+    yaml.dump(device_list, myfile, default_flow_style=True, indent=4)
+
+with open('r', myfile) as fin:
+    device_list = yaml.load(fin)
 pprint(device_list)
+
