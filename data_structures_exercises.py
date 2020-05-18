de8dd5645fec961a20d0ef42db5c8d2223df5cec
Patrick Herrington <pherrington3@gatech.edu>
2020-05-18 11:40:11 -0700

    ex7 initial - check format of config

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -7,17 +7,25 @@ import yaml
 from netmiko import ConnectHandler
 from ciscoconfparse import CiscoConfParse
 
-myfile = '../.netmiko.yml'
-with open(myfile, 'r') as fin:
-    device_list = yaml.load(fin)
-cisco4 = device_list['cisco4']
-net_connect = ConnectHandler(**cisco4)
-show_run_output = net_connect.send_command("show run")
+bgp_config_snippet = """
+router bgp 44
+ bgp router-id 10.220.88.38
+ address-family ipv4 unicast
+ !
+ neighbor 10.220.88.20
+  remote-as 42
+  description pynet-rtr1
+  address-family ipv4 unicast
+   route-policy ALLOW in
+   route-policy ALLOW out
+  !
+ !
+ neighbor 10.220.88.32
+  remote-as 43
+  address-family ipv4 unicast
+   route-policy ALLOW in
+   route-policy ALLOW out
+"""
 
-cisco_cfg = CiscoConfParse(show_run_output.splitlines())
-interfaces_w_ip = cisco_cfg.find_objects_w_child(parentspec=r'^interface', childspec=r'^\s+ip address')
-for interface in interfaces_w_ip:
-    print('Interface Line:', interface.text)
-    ip_addresses = interface.re_search_children(r'ip address')
-    for address_line in ip_addresses:
-        print('IP Address Line:', address_line.text)
+cisco_cfg = CiscoConfParse(bgp_config_snippet.splitlines())
+pprint(cisco_cfg.text)
