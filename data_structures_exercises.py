7ae1ab3a51bbe7b1089f91fb72a9a52c2e5d9373
Patrick Herrington <pherrington3@gatech.edu>
2020-05-18 11:12:21 -0700

    ex5 initial read - not complete

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -3,20 +3,9 @@
 PyPlus Class 3 Exercises
 """
 from pprint import pprint
-import json
+import yaml
 
-device_list = [
-    {'device_name': 'cisco3', 'hostname': 'cisco3.lasthop.io', 'username': 'myname', 'password': 'mypass'},
-    {'device_name': 'cisco4', 'hostname': 'cisco4.lasthop.io', 'username': 'myname', 'password': 'mypass'},
-    {'device_name': 'arista1', 'hostname': 'arista1.lasthop.io', 'username': 'myname', 'password': 'mypass'},
-    {'device_name': 'arista2', 'hostname': 'arista2.lasthop.io', 'username': 'myname', 'password': 'mypass'},
-]
-
-myfile = 'arista_arp.json'
-arp_entries = dict()
+myfile = '~/.netmiko.yml'
 with open(myfile, 'r') as fin:
-    arp_output = json.load(fin)
-v4_neighbors_list = arp_output['ipV4Neighbors']
-for arp_entry in v4_neighbors_list:
-    arp_entries[arp_entry['address']] = arp_entry['hwAddress']
-pprint(arp_entries)
+    device_list = yaml.load(fin)
+pprint(device_list)
