4c9884d78f7dfe1b266558efcda985ac4e21749c
Patrick Herrington <pherrington3@gatech.edu>
2020-05-17 17:21:00 -0700

    ex3

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -3,7 +3,7 @@
 PyPlus Class 3 Exercises
 """
 from pprint import pprint
-import yaml
+import json
 
 device_list = [
     {'device_name': 'cisco3', 'hostname': 'cisco3.lasthop.io', 'username': 'myname', 'password': 'mypass'},
@@ -12,10 +12,18 @@ device_list = [
     {'device_name': 'arista2', 'hostname': 'arista2.lasthop.io', 'username': 'myname', 'password': 'mypass'},
 ]
 
-myfile = 'device_list.yaml'
-with open(myfile, 'w') as fout:
-    yaml.dump(device_list, fout, default_flow_style=True, indent=4)
-
+myfile = 'napalm_unit_test.json'
+ipv4_list = []
+ipv6_list = []
 with open(myfile, 'r') as fin:
-    device_list = yaml.load(fin)
-pprint(device_list)
+    napalm_unit_test = json.load(fin)
+for interface, interface_dict in napalm_unit_test.items():
+    for protocol in interface_dict.keys():
+        if protocol == 'ipv4':
+            for ip_address, address_dict in interface_dict[protocol].items():
+                prefix_length = address_dict['prefix_length']
+                ipv4_list.append(ip_address + '/' + prefix_length)
+        elif protocol == 'ipv6':
+            for ip_address, address_dict in interface_dict[protocol].items():
+                prefix_length = address_dict['prefix_length']
+                ipv6_list.append(ip_address + '/' + prefix_length)
