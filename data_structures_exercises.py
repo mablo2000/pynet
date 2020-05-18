55c2366f3ba26764efbb4be9fea983aa280a667f
Patrick Herrington <pherrington3@gatech.edu>
2020-05-18 11:30:05 -0700

    ex6 initial - reading format

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -5,12 +5,19 @@ PyPlus Class 3 Exercises
 from pprint import pprint
 import yaml
 from netmiko import ConnectHandler
-
+from CiscoConfParse import CiscoConfParse
 
 myfile = '../.netmiko.yml'
 with open(myfile, 'r') as fin:
     device_list = yaml.load(fin)
-cisco3 = device_list['cisco3']
-net_connect = ConnectHandler(**cisco3)
-output = net_connect.find_prompt()
-pprint(output)
+cisco4 = device_list['cisco4']
+net_connect = ConnectHandler(**cisco4)
+show_run_output = net_connect.send_command("show run")
+
+cisco_cfg = CiscoConfParse(show_run_output.splitlines())
+interfaces_w_ip = cisco_cfg.find_objects_w_child(parentspec=r'^interface', childspec=r'^/s+ip address')
+
+for interface in interfaces_w_ip:
+    print('Interface Line:', interface.text)
+    for childline in interface.children:
+        print(childline)
