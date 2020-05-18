38de73f38cab29768eba50e3a2e2deba78e24190
Patrick Herrington <pherrington3@gatech.edu>
2020-05-18 11:13:06 -0700

    path to file

--- data_structures_exercises.py
+++ data_structures_exercises.py
@@ -5,7 +5,7 @@ PyPlus Class 3 Exercises
 from pprint import pprint
 import yaml
 
-myfile = '~/.netmiko.yml'
+myfile = '../.netmiko.yml'
 with open(myfile, 'r') as fin:
     device_list = yaml.load(fin)
 pprint(device_list)
