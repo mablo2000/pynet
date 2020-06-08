41b08246e40bdf63e6a36984a959b71a4a26f9e6
Patrick Herrington <pherrington3@gatech.edu>
2020-06-07 22:58:32 -0700

    class5ex2b.  Expansion of 2a to add BGP config

---
+++ class5ex2b.py
@@ -0,0 +1,15 @@
+from jinja2 import FileSystemLoader, Environment
+
+nxos1 = {'interface': 'Ethernet1/1', 'ip_address': '10.1.100.1', 'netmask': '24', 'local_as': '22',
+         'peer_ip': '10.1.100.2'}
+nxos2 = {'interface': 'Ethernet1/1', 'ip_address': '10.1.100.2', 'netmask': '24', 'local_as': '22',
+         'peer_ip': '10.1.100.1'}
+
+env = Environment()
+env.loader = FileSystemLoader("./templates/")
+template = 'class5ex2b.j2'
+
+for interface_info in (nxos1, nxos2):
+    template = env.get_template(template)
+    output = template.render(**interface_info)
+    print(output)
